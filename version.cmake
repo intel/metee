848e2755f3cad0d16c9bd45ef535b0038b0ac567
Alexander Usyskin <alexander.usyskin@intel.com>
2019-11-19 11:48:00 +0200

    bump version to 2.2.0
    Fix version file parser to handle newlines and
    bump version to 2.2.0
    
    Signed-off-by: Alexander Usyskin <alexander.usyskin@intel.com>

--- version.cmake
+++ version.cmake
@@ -2,6 +2,7 @@
 # Copyright (C) 2014-2019 Intel Corporation
 
 file (READ VERSION VER_FILE)
+string (STRIP "${VER_FILE}" VER_FILE)
 string (REPLACE "." ";" VER_LIST ${VER_FILE})
 list (GET VER_LIST 0 TEE_VERSION_MAJOR)
 list (GET VER_LIST 1 TEE_VERSION_MINOR)
