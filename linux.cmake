e67eb12de19f259fc2ab5015e4e4b0e645023e51
Tomas Winkler <tomas.winkler@intel.com>
2020-01-15 22:35:03 +0200

    MeTee: use target_ functions in linux.cmake
    Use modern target_ cmake functions in linux.cmake.
    
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>

--- linux.cmake
+++ linux.cmake
@@ -5,17 +5,15 @@ set(TEE_SOURCES src/linux/metee_linux.c src/linux/mei.c)
 add_library(${PROJECT_NAME} STATIC ${TEE_SOURCES})
 
 target_include_directories(${PROJECT_NAME}
-  PRIVATE src/linux
+    PRIVATE src/linux
 )
 
 #More warnings and warning-as-error
-target_compile_options (${PROJECT_NAME} PRIVATE -Wall)
-target_compile_options (${PROJECT_NAME} PRIVATE -Werror)
+target_compile_options(${PROJECT_NAME}
+    PRIVATE -Wall -Werror
+    PRIVATE -fstack-protector-strong -D_FORTIFY_SOURCE=2 -O2 -Wformat -Wformat-security
+)
 
 if(NOT DEFINED CONSOLE_OUTPUT)
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DSYSLOG")
-  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DSYSLOG")
-endif(NOT DEFINED CONSOLE_OUTPUT)
-
-#Secure compile flags
-add_definitions (-fstack-protector-strong -D_FORTIFY_SOURCE=2 -O2 -Wformat -Wformat-security)
+    target_compile_definitions(${PROJECT_NAME} PRIVATE -DSYSLOG)
+endif()
