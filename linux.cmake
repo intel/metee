16a73a453907643a522803a8182fa254d5b48a09
Tomas Winkler <tomas.winkler@intel.com>
2020-04-03 00:34:34 +0300

    MeTee: cmake: make coding style consistent
    Make coding style in the cmake files consistent.
    
    Signed-off-by: Tomas Winkler <tomas.winkler@intel.com>

--- linux.cmake
+++ linux.cmake
@@ -1,22 +1,21 @@
 # SPDX-License-Identifier: Apache-2.0
-# Copyright (C) 2014-2019 Intel Corporation
+# Copyright (C) 2014-2020 Intel Corporation
 set(TEE_SOURCES src/linux/metee_linux.c src/linux/mei.c)
 
 add_library(${PROJECT_NAME} STATIC ${TEE_SOURCES})
 
-target_include_directories(${PROJECT_NAME}
-    PRIVATE src/linux
-)
+target_include_directories(${PROJECT_NAME} PRIVATE src/linux)
 
-#More warnings and warning-as-error
-target_compile_options(${PROJECT_NAME}
-    PRIVATE -Wall -Werror
-    PRIVATE -fstack-protector-strong
-    -D_FORTIFY_SOURCE=2 -O2 -Wformat -Wformat-security
+# More warnings and warning-as-error
+target_compile_options(
+  ${PROJECT_NAME}
+  PRIVATE -Wall -Werror
+  PRIVATE -fstack-protector-strong -D_FORTIFY_SOURCE=2 -O2 -Wformat
+          -Wformat-security
 )
 
 if(NOT DEFINED CONSOLE_OUTPUT)
-    target_compile_definitions(${PROJECT_NAME} PRIVATE -DSYSLOG)
+  target_compile_definitions(${PROJECT_NAME} PRIVATE -DSYSLOG)
 endif()
 
 target_compile_definitions(${PROJECT_NAME} PRIVATE -D_GNU_SOURCE)
