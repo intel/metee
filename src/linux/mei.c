af5df1103407b491aeaaf347697a07937b7e32b3
Tamar Mashiah <tamar.mashiah@intel.com>
2020-05-05 12:33:30 +0300

    MeTee: Linux: dump_hex_buffer() remove variable length array
    Remove variable length array in dump_hex_buffer()
    as those are considered unsafe.
    
    sparse: mei.c:52:19: warning: Variable length array is used.
    Signed-off-by: Tamar Mashiah <tamar.mashiah@intel.com>

--- src/linux/mei.c
+++ src/linux/mei.c
@@ -48,20 +48,23 @@ static inline void __dump_buffer(const char *buf)
 
 static void dump_hex_buffer(const unsigned char *buf, size_t len)
 {
-	const size_t pbufsz = 16 * 3;
-	char pbuf[pbufsz];
+#define LINE_LEN 16
+#define PBUFSZ (sizeof("00") * LINE_LEN)
+	char pbuf[PBUFSZ];
 	int j = 0;
 
 	while (len-- > 0) {
-		snprintf(&pbuf[j], pbufsz - j, "%02X ", *buf++);
+		snprintf(&pbuf[j], PBUFSZ - j, "%02X ", *buf++);
 		j += 3;
-		if (j == 16 * 3) {
+		if (j == PBUFSZ) {
 			__dump_buffer(pbuf);
 			j = 0;
 		}
 	}
 	if (j)
 		__dump_buffer(pbuf);
+#undef PBUFSZ
+#undef LINE_LEN
 }
 
 static void mei_dump_hex_buffer(struct mei *me,
